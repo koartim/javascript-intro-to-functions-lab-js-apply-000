acbd5b1fa0233a273c2d806aee0983f91f79bf2b
Timothy Koar <koartimothy@gmail.com>
2019-03-04 17:11:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === "hello") {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
 }
 
 function sayHiToGrandma(string) {
   if (string === "HELLO") {
-    return 'YES INDEED!'
+    return 'YES INDEED!';
   }
 }
 
 function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma') {
-    return 'I love you too'
+    return 'I love you too';
   }
 }
